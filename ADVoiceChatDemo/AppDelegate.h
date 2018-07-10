9456e8f67a35b4540812a249332ee26395025e7e
César <424321999@qq.com>
2018-07-10 10:44:59 +0800

    Initial Commit

---
+++ ADVoiceChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADVoiceChatDemo
+//
+//  Created by Adu on 2018/7/10.
+//  Copyright © 2018年 Adu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
